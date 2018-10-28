550099af197ed8c0e65c81031972a80c7906b484
aussieninja <44412660+aussieninja@users.noreply.github.com>
2018-10-27 22:56:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const title = <h1>My First React Element</h1>;
 const desc = <p>I just learned out to create a React node and render it into the DOM</p>;
-const sub_heading = <p>Now with more text</p>;
+const sub_heading = <p>Now with more text in a subheading</p>;
 
 const header = React.createElement(
   'header',
