3f3063d4772fc40955c34496bd2870757d723d47
aussieninja <44412660+aussieninja@users.noreply.github.com>
2018-10-27 23:07:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,25 +8,6 @@ const message = (
 <p> Hello there! </p>
 );
 
-const box = (
-      <Column flexGrow={1}>
-        <Row horizontal='center'>
-          <h1>HEADER</h1>
-        </Row>
-        <Row vertical='center'>
-          <Column flexGrow={1} horizontal='center'>
-            <h3> Column 1 </h3>
-            <span> column 1 content </span>
-          </Column>
-          <Column flexGrow={1} horizontal='center'>
-            <h3> Column 2 </h3>
-            <span> column 2 content </span>
-          </Column>
-        </Row>
-      </Column>
-);
-
-
 const header = React.createElement(
   'header',
    null,
@@ -34,8 +15,7 @@ const header = React.createElement(
    desc,
    sub_heading,
    welcome,
-   message,
-   box
+   message
 );
 
 
